c7dc66f86a9034e88261a1b161cb940628ab2c42
andisugandi <andisugandi@gmail.com>
2025-03-27 20:24:46 +0700

    Created Post model test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+    def test_post_model_exists(self):
+        posts = Post.objects.count()
+        self.assertEqual(posts, 0)
+
+    def test_post_model_has_string_representation(self):
+        user = User.objects.create(username='andi@sibermu.id')
+        post = Post.objects.create(title='First post',\
+                                   slug='first-post',\
+                                   body='Post body.',\
+                                   author=user)
+        self.assertEqual(str(post), post.title)
