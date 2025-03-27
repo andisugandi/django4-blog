f515f448f636ebe64206687199b6b2660b81951d
andisugandi <andisugandi@gmail.com>
2025-03-27 23:42:14 +0700

    Created Post list page test

--- blog/tests.py
+++ blog/tests.py
@@ -16,3 +16,22 @@ class PostModelTest(TestCase):
                                    body='Post body.',\
                                    author=user)
         self.assertEqual(str(post), post.title)
+
+
+class PostListTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='andi@sibermu.id')
+        self.post = Post.objects.create(title='First post',\
+                                   slug='first-post',\
+                                   body='Post body.',\
+                                   author=self.user,\
+                                   status='PB')
+
+    def test_post_list_page_returns_correct_response(self):
+        response = self.client.get('/blog/')
+        self.assertTemplateUsed(response, 'blog/post/list.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_page_returns_correct_post_title(self):
+        response = self.client.get('/blog/')
+        self.assertContains(response, self.post.title)
